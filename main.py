1bc41e42efbadd6e71be0325de8a5ae5e0a20d95
Omar Barazanji <ozanji345@gmail.com>
2022-03-17 17:54:35 -0500

    init commit skeleton code

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Given a substring, search an array of strings by cosine simularity (linear kernel) by 
+returning the index with highest kernel output (confidence).
+"""
+
+class NLPSearch:
+
+    def __init__(self):
+        pass
+
+    def process_data(self):
+        pass
+
+    def create_model(self):
+        pass
+
+    def train_model(self):
+        pass
+
+    def prompt(self, prompt):
+        pass
+
+if __name__ == "__main__":
+    search = NLPSearch()
